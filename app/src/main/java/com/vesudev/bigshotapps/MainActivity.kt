54c97c77897c8fdf043af19762b4358639957345
Vesubius <jeffryperezduarte@gmail.com>
2024-02-24 15:38:01 -0600

    Agregado Fondo y botones de camara y galeria

--- app/src/main/java/com/vesudev/bigshotapps/MainActivity.kt
+++ app/src/main/java/com/vesudev/bigshotapps/MainActivity.kt
@@ -2,18 +2,30 @@ package com.vesudev.bigshotapps
 
 
 import android.os.Bundle
+
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 
 import androidx.compose.foundation.layout.padding
 
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.width
+
+
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 
@@ -29,12 +41,18 @@ import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 
 import androidx.compose.runtime.Composable
 
+import androidx.compose.ui.Alignment
+
 import androidx.compose.ui.Modifier
+
+import androidx.compose.ui.res.painterResource
 
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
 import com.vesudev.bigshotapps.ui.theme.BigShotAppsTheme
 
 
@@ -56,8 +74,8 @@ fun MainScreen() {
     Scaffold(
         //====================== BARRA SUPERIOR ========================================
         topBar = {
-
             TopAppBar(
+
                 //Parametro de Top App Bar
                 title = {
 
@@ -71,6 +89,7 @@ fun MainScreen() {
                 // Parametro de Top app bar
                 colors = topAppBarColors(containerColor = MaterialTheme.colorScheme.primary)
             )
+
         },
 
         //====================== CUERPO DE LA APP =================================
@@ -107,19 +126,51 @@ fun MainScreen() {
 }
 
 
+@OptIn(ExperimentalCoilApi::class)
 @Composable
 fun ScreenBody(padding: PaddingValues) {
-    LazyColumn(
+    Box(modifier = Modifier.fillMaxSize()) {
+        // Imagen de fondo
+        Image(
+            painter = rememberImagePainter("https://i.pinimg.com/564x/ed/1a/36/ed1a36532ba9a37f71add3dc9d03743f.jpg"),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize()
+        )
 
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(padding)
-            .padding(top = 62.dp)
-            .padding(bottom = 80.dp)
-    ) {
+        // ======================== Contenido de la Pantalla ==========================
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            Row (modifier = Modifier.padding(bottom = 100.dp)){
+
+                //------------------- Boton Camara ----------------------------------------
+                Image(
+                    modifier = Modifier
+                        .height(80.dp)
+                        .width(80.dp),
+                    painter = painterResource(id = R.drawable.camera),
+                    contentDescription = "Boton Camara",
+                )
+                Spacer(modifier= Modifier.width(20.dp))
+
+                //------------------- Boton Galeria ----------------------------------------
+                Image(
+                    modifier = Modifier
+                        .height(80.dp)
+                        .width(80.dp),
+                    painter = painterResource(id = R.drawable.galery),
+                    contentDescription = "Boton Camara",
+                )
+            }
 
 
+        }
     }
+
 }
 
 
