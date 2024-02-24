8439256e4192277f4503f719825636ad59fa8f9e
Vesubius <jeffryperezduarte@gmail.com>
2024-02-24 10:25:49 -0600

    Plantilla inicial Big Shot App

--- app/src/main/java/com/vesudev/bigshotapps/MainActivity.kt
+++ app/src/main/java/com/vesudev/bigshotapps/MainActivity.kt
@@ -1,49 +1,42 @@
 package com.vesudev.bigshotapps
 
-import android.content.ClipData.Item
-import android.content.res.Configuration
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import android.icu.text.CaseMap.Title
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
+
 
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
+
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
+
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
+
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
-import androidx.compose.material3.contentColorFor
+
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.vesudev.bigshotapps.ui.theme.BigShotAppsTheme
-import androidx.compose.material3.ButtonColors as ButtonColors1
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -86,7 +79,7 @@ fun MainScreen() {
         //--------------------------Boton Flotante----------------------------
         floatingActionButton = {
 
-            Button(onClick = { /*TODO*/ }){
+            Button(onClick = { /*TODO*/ }) {
                 Text(text = "Subir foto")
             }
 
@@ -114,13 +107,13 @@ fun MainScreen() {
 }
 
 
-
 @Composable
 fun ScreenBody(padding: PaddingValues) {
     LazyColumn(
 
         modifier = Modifier
             .fillMaxSize()
+            .padding(padding)
             .padding(top = 62.dp)
             .padding(bottom = 80.dp)
     ) {
@@ -130,6 +123,7 @@ fun ScreenBody(padding: PaddingValues) {
 }
 
 
+//
 @Preview
 @Composable
 fun PreviewMainScreen() {
