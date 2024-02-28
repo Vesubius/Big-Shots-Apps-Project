e0c6e46972f36462ffc343d4cdb339dc9fd135ce
Vesubius <jeffryperezduarte@gmail.com>
2024-02-28 09:18:24 -0600

    Correcciones visuales

--- app/src/main/java/com/vesudev/bigshotapps/MainActivity.kt
+++ app/src/main/java/com/vesudev/bigshotapps/MainActivity.kt
@@ -11,10 +11,13 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
@@ -30,11 +33,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
+
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.compose.rememberImagePainter
@@ -136,27 +140,33 @@ fun ScreenBody(padding: PaddingValues, stateImage: Uri?) {
             contentDescription = "Fondo",
             modifier = Modifier.fillMaxSize()
         )
+        Column(modifier = Modifier
+            .height(400.dp)
+            .fillMaxWidth()
+            .padding(horizontal = 30.dp)) {
+            // ======================== Contenido de la Pantalla ==========================
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding),
 
-        // ======================== Contenido de la Pantalla ==========================
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(padding),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Bottom
-        ) {
+                verticalArrangement = Arrangement.Bottom
+            ) {
 
-            item {
-                AsyncImage(
-                    model = stateImage,
-                    contentDescription = null,
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier.fillMaxWidth()
-                )
+                item {
+                    AsyncImage(
+                        model = stateImage,
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+
+
             }
-
-
         }
+
+
     }
 
 }
