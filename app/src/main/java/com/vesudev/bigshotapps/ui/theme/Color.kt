212b49d942e4efa09bfda0de2adc87ae14c66252
Vesubius <jeffryperezduarte@gmail.com>
2024-03-03 10:44:48 -0600

    comentando codigo

--- app/src/main/java/com/vesudev/bigshotapps/ui/theme/Color.kt
+++ app/src/main/java/com/vesudev/bigshotapps/ui/theme/Color.kt
@@ -10,5 +10,6 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+// ===========  Colores  ========================
 val BlueBackground = Color(0xFF306E8C)
 val DarkGrey = Color(0xFF404040)
